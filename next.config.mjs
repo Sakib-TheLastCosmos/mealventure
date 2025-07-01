62ce2915d88780a236e1755090d4de2daa8dd862
Sakib UL Hasan <sakibulhasan159@gmail.com>
2025-07-01 20:52:33 +0600

    Fixed export errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/mealventure",
-  assetPrefix: "/mealventure", // <-- Add this line
-  trailingSlash: true,
-
   eslint: {
     ignoreDuringBuilds: true,
   },
